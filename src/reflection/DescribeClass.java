58f7b78b20cdd56f55fc4448a821045522cb98e2
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-27 10:46:36 +0000

    nit: Tests logic to use CLI args

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,9 +1,11 @@
 package reflection;
 
+import java.util.Arrays;
+
 public class DescribeClass {
     public static void main(String[] args) {
         if (args.length != 1)
-            System.out.println("Usage: "); // TODO: describe how to use the utility
+            System.out.println("Usage: " + Arrays.toString(args)); // TODO: describe how to use the utility
 
         // TODO: implement the functionality for Question 1
     }
