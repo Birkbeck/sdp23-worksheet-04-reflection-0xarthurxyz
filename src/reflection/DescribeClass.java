c31a19d7f4aa12cebd1ff71b0f24d9427c8401c1
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-27 11:07:45 +0000

    nit: Testing how java.lang.Class works

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -4,9 +4,19 @@ import java.util.Arrays;
 
 public class DescribeClass {
     public static void main(String[] args) {
-        if (args.length != 1)
+        if ( args.length == 0 ) {
+            System.out.println("Argument count: " + args.length + ", too few args supplied.");
+        } else {
             System.out.println("Usage: " + Arrays.toString(args)); // TODO: describe how to use the utility
+        }
 
         // TODO: implement the functionality for Question 1
+        try {
+            Class<?> suppliedClass = Class.forName(args[0]);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
