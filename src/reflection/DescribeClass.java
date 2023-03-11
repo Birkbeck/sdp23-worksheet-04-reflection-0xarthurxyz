9050ad51dc33a497976c8345ee0819b33055716e
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-11 18:29:38 +0000

    feat: emits methods of supplied class name

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,18 +1,34 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class DescribeClass {
     public static void main(String[] args) {
-        if ( args.length == 0 ) {
-            System.out.println("Argument count: " + args.length + ", too few args supplied.");
+        // Attribute(s)
+        String suppliedClassName;
+
+        // Validates inputs from command line
+        if ( args.length == 1 ) {
+            suppliedClassName = args[0]; // e.g. "java.lang.String"
+            System.out.println("Usage: " + suppliedClassName);
         } else {
-            System.out.println("Usage: " + Arrays.toString(args)); // TODO: describe how to use the utility
+            System.out.println("Argument count: " + args.length + ", max 1 argument.");
+            return;
         }
 
-        // TODO: implement the functionality for Question 1
+        // - [ ] TODO: implement the functionality for Question 1
+        /* Notes:
+         * - [ ] emit the interface of the class in Java syntax
+         */
         try {
-            Class<?> suppliedClass = Class.forName(args[0]);
+            // Gets class using class loader
+            Class<?> suppliedClass = Class.forName(suppliedClassName);
+
+            // Test: Confirm class loader works as expected
+            Method[] methods = suppliedClass.getMethods();
+            System.out.println(Arrays.toString(methods));
+
         } catch (ClassNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
