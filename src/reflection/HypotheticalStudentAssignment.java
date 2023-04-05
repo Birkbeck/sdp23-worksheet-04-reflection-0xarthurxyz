b25847d6438666460e0144028ae3926ffee280d4
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-04-05 21:29:01 +0100

    feat(hypothetical class): implements class to try and satisfy all tests

--- src/reflection/HypotheticalStudentAssignment.java
+++ src/reflection/HypotheticalStudentAssignment.java
@@ -1,5 +1,43 @@
 package reflection;
 
 public class HypotheticalStudentAssignment {
-	
+	// Fields
+	private String privateField1;
+	private String privateField2;
+	private String privateField3;
+	private String privateField4;
+	private String privateField5; // Fail because there are more than four fields
+	public String publicField5; // Fail because there is a non-private field
+	// Fail because there is a field of type `ArrayList`
+	private java.util.ArrayList<Integer> arrayListField; 
+
+	// Helper methods
+	// Fail because there are fewer than two private helper methods
+	private void onlyPrivateHelperMethod() {
+		System.out.println("Hello, world!");
+	}
+
+	// Fail because method returns an `int`
+	public int publicMethodThatReturnsInteger() {
+		return 0;
+	}
+
+	// Fail because method has a `throws` clause
+	public void publicMethodThatThrowsException() throws Exception {
+		throw new Exception("Hello exception!");
+	}
+
+	// Constructor
+	// Fail because there is no zero-argument constructor
+	public HypotheticalStudentAssignment() {
+		this.privateField1 = "Hello";
+		this.privateField2 = "world";
+		this.privateField3 = "!";
+		this.privateField4 = "More";
+		this.privateField5 = "than four fields";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
 }
