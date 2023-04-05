f0f0c42c0e94a96db1f749943219d1401ba21923
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-04-05 20:17:39 +0100

    feat(autograder): adds new java class to apply autograder tests

--- test/reflection/Autograder.java
+++ test/reflection/Autograder.java
@@ -4,17 +4,49 @@ import org.junit.jupiter.api.Test;
 
 public class Autograder {
 
-    static final String CLASS_NAME = ""; // TODO: insert the class name to be tested
+    static final String CLASS_NAME = "HypotheticalStudentAssignment.java";
+
+    // Make the tests fail if the class under test has any of the following:
+    //    + more than four fields,
+    //    + any non-private fields,
+    //    + any fields of type `ArrayList`,
+    //    + fewer than two private *helper* methods, 
+    //    + any method that has a `throws` clause, 
+    //    + any method that returns an `int`,
+    //    + missing a zero-argument constructor.
 
     @Test
-    public void testMoreThanFourFields() {
+    public void givenClassName_whenClassHasMoreThanFourFields_thenFail() {
         // TODO: add testing code
     }
 
     @Test
-    public void testNonPrivateFields() {
+    public void givenClassName_whenClassHasNonPrivateFields_thenFail() {
         // TODO: add testing code
     }
 
-    // TODO: add a test for each of the remaining items of Question 3
+    @Test
+    public void givenClassName_whenClassHasFieldOfTypeArrayList_thenFail() {
+        // TODO: add testing code
+    }
+
+    @Test
+    public void givenClassName_whenClassHasFewerThanTwoPrivateHelperMethods_thenFail() {
+        // TODO: add testing code
+    }
+    
+    @Test
+    public void givenClassName_whenClassHasAnyMethodThatReturnsAnInteger_thenFail() {
+        // TODO: add testing code
+    }
+    
+    @Test
+    public void givenClassName_whenClassHasAnyMethodWithThrowsClause_thenFail() {
+        // TODO: add testing code
+    }
+
+    @Test
+    public void givenClassName_whenClassIsMissingZeroArgumentConstructor_thenFail() {
+        // TODO: add testing code
+    }
 }
