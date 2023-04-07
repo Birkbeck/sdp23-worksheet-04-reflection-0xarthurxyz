52c335527871c1a5dd772ed2c99f60631b4ef784
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-04-07 22:37:32 +0100

    feat(ToString): adds complete code to print fields

--- src/reflection/ToString.java
+++ src/reflection/ToString.java
@@ -2,7 +2,61 @@ package reflection;
 
 public class ToString {
     public static String toString(Object obj) {
-        // TODO: implement the functionality for Question 4
-        return null;
+        // Loads class of `obj`
+        Class<?> suppliedClass = obj.getClass();
+
+        // Get declared fields in `obj` class
+        java.lang.reflect.Field[] fields = suppliedClass.getDeclaredFields();
+
+        // Initialise string to return
+        String stringRepresentation = "";
+
+        // Get values of declared fields
+        for (java.lang.reflect.Field field : fields) {
+            try {
+                // Makes all fields accessible (including private fields)
+                field.setAccessible(true);
+                
+                // Adds field modifier to string representation
+                // stringRepresentation += java.lang.reflect.Modifier
+                //         .toString(field.getModifiers())
+                //         + " ";
+
+                // Adds field name to string representation
+                stringRepresentation += field.toString() + " ";
+
+                // Adds field value to string representation
+                stringRepresentation += field.get(obj);
+            } catch (NullPointerException e) {
+                // If the specified object is null and the field is an instance field.
+                // Adds field value as null to string representation
+                stringRepresentation += "null" + " ";
+            } catch (IllegalAccessException e) {
+                // If this Field object is enforcing Java language access control and the
+                // underlying field is inaccessible.
+                // Adds field value as inaccessible to string representation
+                // Should never happen as we set all fields to be accessible
+                stringRepresentation += "inaccessible" + " ";
+            } catch (IllegalArgumentException e) {
+                // If the specified object is not an instance of the class or interface
+                // declaring the underlying field (or a subclass or implementor thereof).
+                stringRepresentation += "not instance of class" + " ";
+            } catch (ExceptionInInitializerError e) {
+                // If the initialization provoked by this method fails.
+                stringRepresentation += "initialization failed" + " ";
+            } catch (Exception e) {
+                // If any other exception
+                stringRepresentation += "unknown exception" + " ";
+            }
+            // Adds new line to for each field
+            stringRepresentation += "\n";
+        }
+
+        // Remove trailing space
+        stringRepresentation = stringRepresentation
+                .substring(0, stringRepresentation.length() - 1);
+
+        // Return beautified string version of `obj`
+        return stringRepresentation;
     }
 }
