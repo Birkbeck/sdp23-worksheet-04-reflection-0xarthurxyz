cb90e43cea3fb497ab09010ce238cd3561b0d80b
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-04-07 22:47:09 +0100

    feat(ToString): adds complementary information and more descriptive text to string representation

--- src/reflection/ToString.java
+++ src/reflection/ToString.java
@@ -2,54 +2,67 @@ package reflection;
 
 public class ToString {
     public static String toString(Object obj) {
-        // Loads class of `obj`
-        Class<?> suppliedClass = obj.getClass();
-
-        // Get declared fields in `obj` class
-        java.lang.reflect.Field[] fields = suppliedClass.getDeclaredFields();
-
-        // Initialise string to return
+        // Initialise empty string representation
         String stringRepresentation = "";
 
-        // Get values of declared fields
-        for (java.lang.reflect.Field field : fields) {
-            try {
-                // Makes all fields accessible (including private fields)
-                field.setAccessible(true);
-                
-                // Adds field modifier to string representation
-                // stringRepresentation += java.lang.reflect.Modifier
-                //         .toString(field.getModifiers())
-                //         + " ";
+        // Loads class that `obj` is an instance of
+        Class<?> suppliedClass = obj.getClass();
 
-                // Adds field name to string representation
-                stringRepresentation += field.toString() + " ";
+        // Adds class name that `obj` is an instance of to string representation
+        stringRepresentation += "Instance of: "
+                + suppliedClass.getName()
+                + "\n";
 
-                // Adds field value to string representation
-                stringRepresentation += field.get(obj);
-            } catch (NullPointerException e) {
-                // If the specified object is null and the field is an instance field.
-                // Adds field value as null to string representation
-                stringRepresentation += "null" + " ";
-            } catch (IllegalAccessException e) {
-                // If this Field object is enforcing Java language access control and the
-                // underlying field is inaccessible.
-                // Adds field value as inaccessible to string representation
-                // Should never happen as we set all fields to be accessible
-                stringRepresentation += "inaccessible" + " ";
-            } catch (IllegalArgumentException e) {
-                // If the specified object is not an instance of the class or interface
-                // declaring the underlying field (or a subclass or implementor thereof).
-                stringRepresentation += "not instance of class" + " ";
-            } catch (ExceptionInInitializerError e) {
-                // If the initialization provoked by this method fails.
-                stringRepresentation += "initialization failed" + " ";
-            } catch (Exception e) {
-                // If any other exception
-                stringRepresentation += "unknown exception" + " ";
+        // Gets declared fields in `obj` class
+        java.lang.reflect.Field[] fields = suppliedClass.getDeclaredFields();
+
+        if (fields.length == 0 || fields == null) {
+            // If `obj` has no fields
+            return "No fields";
+        } else {
+            // If `obj` has fields
+            // Adds number of fields to string representation
+            stringRepresentation += "Number of fields: "
+                    + fields.length
+                    + "\n";
+
+            stringRepresentation += "Fields and values: \n";
+            
+            // Adds each field name and value to string representation
+            for (java.lang.reflect.Field field : fields) {
+                try {
+                    // Makes all fields accessible (including private fields)
+                    field.setAccessible(true);
+
+                    // Adds field name to string representation
+                    stringRepresentation += field.toString() + ": ";
+
+                    // Adds field value to string representation
+                    stringRepresentation += field.get(obj);
+                } catch (NullPointerException e) {
+                    // If the specified object is null and the field is an instance field.
+                    // Adds field value as null to string representation
+                    stringRepresentation += "null" + " ";
+                } catch (IllegalAccessException e) {
+                    // If this Field object is enforcing Java language access control and the
+                    // underlying field is inaccessible.
+                    // Adds field value as inaccessible to string representation
+                    // Should never happen as we set all fields to be accessible
+                    stringRepresentation += "inaccessible" + " ";
+                } catch (IllegalArgumentException e) {
+                    // If the specified object is not an instance of the class or interface
+                    // declaring the underlying field (or a subclass or implementor thereof).
+                    stringRepresentation += "not instance of class" + " ";
+                } catch (ExceptionInInitializerError e) {
+                    // If the initialization provoked by this method fails.
+                    stringRepresentation += "initialization failed" + " ";
+                } catch (Exception e) {
+                    // If any other exception
+                    stringRepresentation += "unknown exception" + " ";
+                }
+                // Adds new line to for each field
+                stringRepresentation += "\n";
             }
-            // Adds new line to for each field
-            stringRepresentation += "\n";
         }
 
         // Remove trailing space
