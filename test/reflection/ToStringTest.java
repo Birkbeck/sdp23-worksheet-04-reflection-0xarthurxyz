a6a539471b607bf0ec78c36186d539adb8d3e7ae
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-04-07 22:38:39 +0100

    test(ToString): adds incomplete test

--- test/reflection/ToStringTest.java
+++ test/reflection/ToStringTest.java
@@ -1,10 +1,17 @@
 package reflection;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import reflection.ToStringExampleClasses.PublicStringFields;
 
 public class ToStringTest {
 
-    // TODO: write tests for ToString.toString
+    @Test
+    public void givenClassWithPublicStringFields_whenToString_thenReturnStringVersion() {
+        Assertions.assertEquals("String publicStringField1: Hello, String publicStringField2: World", 
+                                ToString.toString(new PublicStringFields("Hello", "World")));
+    }
 
     @Test
     public void testToString1() {
